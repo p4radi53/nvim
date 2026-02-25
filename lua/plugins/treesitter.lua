934d08d93ec2b1d468964241d77da42fe8c15194
p4radi53 <adam.raj27@gmail.com>
2026-02-25 19:13:25 +0100

    Format treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,36 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      ---@diagnostic disable-next-line: missing-fields
-      configs.setup({
-        ensure_installed = {
-          "c",
-          "lua",
-          "vim",
-          "python",
-          "rust",
-          "java",
-          "javascript",
-          "typescript",
-          "html",
-          "css",
-          "terraform",
-          "hcl",
-          "yaml",
-          "toml",
-          "json",
-          "markdown",
-          "go",
-          "just",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+			---@diagnostic disable-next-line: missing-fields
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"python",
+					"rust",
+					"java",
+					"javascript",
+					"typescript",
+					"html",
+					"css",
+					"terraform",
+					"hcl",
+					"yaml",
+					"toml",
+					"json",
+					"markdown",
+					"go",
+					"just",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
