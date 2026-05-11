cd237762f017946d3960e11ca3180dc4fb5aa64f
Adam Raj <adaraj@inpost.pl>
2026-05-11 23:31:42 +0200

    Lock blink.cmp to v1, add cargo build on install/update

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -11,8 +11,27 @@ end
 
 -- Blink CMP
 vim.pack.add({
-	{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.0") },
+	{ src = "https://github.com/saghen/blink.cmp", version = "v1" },
 	{ src = "https://github.com/rafamadriz/friendly-snippets" },
+})
+vim.api.nvim_create_autocmd("PackChanged", {
+	callback = function(ev)
+		local name, kind = ev.data.spec.name, ev.data.kind
+		if name == "blink.cmp" and (kind == "install" or kind == "update") then
+			-- hardcoded path; update if vim.pack install_dir changes
+			local dir = vim.fn.stdpath("data") .. "/site/pack/core/opt/blink.cmp"
+			vim.notify("blink.cmp: building...", vim.log.levels.INFO)
+			vim.system({ "cargo", "build", "--release" }, { cwd = dir }, function(result)
+				vim.schedule(function()
+					if result.code ~= 0 then
+						vim.notify("blink.cmp build failed:\n" .. result.stderr, vim.log.levels.ERROR)
+					else
+						vim.notify("blink.cmp built successfully", vim.log.levels.INFO)
+					end
+				end)
+			end)
+		end
+	end,
 })
 local sources = { "lsp", "path", "snippets", "buffer" }
 local providers = {}
