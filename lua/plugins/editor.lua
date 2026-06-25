c3b99747269682d0e10d556d9eff57df1db4abfb
p4radi53 <adam.raj27@gmail.com>
2026-06-25 05:38:56 +0200

    Remove todo-comments plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -52,20 +52,6 @@ require("conform").setup({
 	format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 })
 
--- Todo Comments
-vim.pack.add({ { src = "https://github.com/folke/todo-comments.nvim" } })
-require("todo-comments").setup()
-vim.keymap.set("n", "]t", function()
-	require("todo-comments").jump_next()
-end, { desc = "Next Todo Comment" })
-vim.keymap.set("n", "[t", function()
-	require("todo-comments").jump_prev()
-end, { desc = "Previous Todo Comment" })
-vim.keymap.set("n", "<leader>xt", "<cmd>Trouble todo toggle<cr>", { desc = "Todo Comments (Trouble)" })
-vim.keymap.set("n", "<leader>ft", function()
-	Snacks.picker.todo_comments()
-end, { desc = "Todo Comments" })
-
 -- Trouble
 vim.pack.add({ { src = "https://github.com/folke/trouble.nvim" } })
 require("trouble").setup()
